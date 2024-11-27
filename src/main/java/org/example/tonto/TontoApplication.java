3e2f3abe1144cf714e970354169c1592eb5cf97b
Egemen Erin <113554575+EgemenErin@users.noreply.github.com>
2024-11-27 16:42:25 +0100

    Add files via upload

---
+++ src/main/java/org/example/tonto/TontoApplication.java
@@ -0,0 +1,13 @@
+package org.example.tonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TontoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TontoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tonto/TontoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TontoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
